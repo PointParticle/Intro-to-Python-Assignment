50e98e32ec3c54a5ee4370726619869913e4c56b
PointParticle <168024488+PointParticle@users.noreply.github.com>
2024-08-14 15:25:54 +0200

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,9 @@
+
+# User details
+
+name = input("What is your name?");
+age = input("How old are you?");
+location = input("Where do you live?");
+food = input("What is your favourite food?")
+print("hello", name,"you are " + age + " old and live in", location + ".", "You enjoy eating " + food + ".");
+
